3f0688327a721534dfba8aac6eb447e65f4be6ac
abnatop <abnatop@gmail.com>
2020-12-06 21:57:55 +0900

    Задача 2.

--- taks_2.py
+++ taks_2.py
@@ -5,7 +5,13 @@
 
 FILE_MODE = 'r'
 f_name = 'task_2.txt'
+line_count = 1
 
 with open(f_name, FILE_MODE) as f_obj:
     for line in f_obj:
-        print(line)
+        words_in_line = len(line.split())
+        print(f'Строка {line_count} содержит слов - {words_in_line}.')
+        line_count += 1
+
+print('---')
+print(f'Файл {f_name} содержит {line_count - 1} строк.')
