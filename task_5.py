4593002d7e5f6f8be5503ae2280b7007c354d5eb
abnatop <abnatop@gmail.com>
2020-12-07 14:30:30 +0900

    Задача 5. Edit .gitignore.

--- task_5.py
+++ task_5.py
@@ -10,12 +10,11 @@ APPEND_MODE = 'a'
 
 RAND_FROM = 0
 RAND_TO = 100
+DIGIT_COUNT = 10
 
 f_name = 'task_5.txt'
 
-for i in range(10):
-    print(randint(0,2))
-
-# with open(f_name, APPEND_MODE) as f_obj:
-#     for line in f_list:
-#         f_obj.write(line + '\n')
+with open(f_name, APPEND_MODE) as f_obj:
+    for i in range(DIGIT_COUNT):
+        digit = randint(RAND_FROM, RAND_TO)
+        f_obj.write(f'{digit} ')
