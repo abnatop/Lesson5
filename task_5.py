4c9a36e619fb673b168b4402acd31129612d0bfe
abnatop <abnatop@gmail.com>
2020-12-07 14:40:59 +0900

    Задача 5. Minor changes.

--- task_5.py
+++ task_5.py
@@ -18,3 +18,10 @@ with open(f_name, APPEND_MODE) as f_obj:
     for i in range(DIGIT_COUNT):
         digit = randint(RAND_FROM, RAND_TO)
         f_obj.write(f'{digit} ')
+
+with open(f_name, READ_MODE) as f_obj:
+    for line in f_obj:
+        digits = line.split()
+
+digits_sum = sum(map(int, digits))
+print(f'Сумма чисел в файле {f_name} равна {digits_sum}.')
