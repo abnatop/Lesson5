41ac0c8270288643d61fdbf7a283c2a828b9340b
abnatop <abnatop@gmail.com>
2020-12-07 14:24:11 +0900

    Задача 5. Initial commit.

--- task_5.py
+++ task_5.py
@@ -3,4 +3,19 @@
 разделенных пробелами. Программа должна подсчитывать сумму чисел в файле и выводить
 ее на экран.
 """
+from random import randint
 
+READ_MODE = 'r'
+APPEND_MODE = 'a'
+
+RAND_FROM = 0
+RAND_TO = 100
+
+f_name = 'task_5.txt'
+
+for i in range(10):
+    print(randint(0,2))
+
+# with open(f_name, APPEND_MODE) as f_obj:
+#     for line in f_list:
+#         f_obj.write(line + '\n')
